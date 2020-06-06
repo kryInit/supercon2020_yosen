d657c49765e6b7a7e757cc06328df94ef8a06f24
kryInit <hgn7qerg@gmail.com>
2020-06-06 09:05:18 +0900

    fix is_NO_case()

--- utility/_is_NO_case.cpp
+++ utility/_is_NO_case.cpp
@@ -2,10 +2,10 @@ bool is_NO_case(string s, string t) {
     int scnt[A2Z_NUM] = {};
     for(auto i : s) scnt[i-'a']++;
 
-    bool dp[MAX_N+1] = {true};
+    bool dp[MAX_T+1] = {true};
     for(int i=0; i<A2Z_NUM; ++i) {
         int ith_scnt = scnt[i];
-        for(int j=ith_scnt; j<=MAX_N; ++j) {
+        for(int j=ith_scnt; j<=MAX_T; ++j) {
             dp[j] |= dp[j-ith_scnt];
         }
     }
